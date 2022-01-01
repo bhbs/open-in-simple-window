63a3ed604f3b4f51cfd6375dd071c7dd3deb6927
bhbs <bhbstar.me@gmail.com>
2022-01-01 21:40:27 +0900

    feat: install typescript

--- src/popup.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("button").addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, {});
-  });
-});
---
+++ src/popup.ts
@@ -0,0 +1,6 @@
+document.querySelector("button")?.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const id = tabs[0].id;
+    if (id) chrome.tabs.sendMessage(id, {});
+  });
+});
